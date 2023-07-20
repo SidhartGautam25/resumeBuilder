b787844515b46b9a285cc9570aa4139e7ae9f165
navneet nishchal <navneetnishchal1420@gmail.com>
2023-07-20 20:58:14 +0530

    download feature added

--- pages/tempone/index.jsx
+++ pages/tempone/index.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react'
+import {ReactToPrint,useReactToPrint} from 'react-to-print';
 import styles from './tempone.module.css'
 import Onetwo from '@/app/components/temponetwo/Onetwo';
 import Formtwo from '@/app/components/formtwo/formtwo';
@@ -19,16 +20,16 @@ export default function Index() {
         "username": "Situ Kumar",
         "gmail": "navneetnishchal1420@gmail.com",
         "city": "delhi",
-        "state":"india"
+        "state": "india"
     })
-    const [formone,setFormone]=useState({
-        "username":names.username,
-        "gmail":names.gmail,
-        "city":names.city,
-        "state":names.state
+    const [formone, setFormone] = useState({
+        "username": names.username,
+        "gmail": names.gmail,
+        "city": names.city,
+        "state": names.state
     })
     const [firstdiv, setFirstdiv] = useState(false);
-    
+
     function funone() {
         setFirstdiv(true);
     }
@@ -38,146 +39,186 @@ export default function Index() {
         setNames(formone);
         setFirstdiv(false);
     }
-    function crossfunctionone(){
+    function crossfunctionone() {
         setFirstdiv(false);
+        check()
+
     }
     const handleChangeone = (event) => {
         const { name, value } = event.target;
         setFormone((prevFormData) => ({ ...prevFormData, [name]: value }));
-      };
-      //first form work completted
+    };
+    //first form work completted
 
     //second form 
-    const [secdiv,setSecdiv]=useState(false);
-    const [ind,setInd]=useState(-1);
-    function setind(index){
+    const [secdiv, setSecdiv] = useState(false);
+    const [ind, setInd] = useState(-1);
+    function setind(index) {
         setInd(index);
         // console.log("index changed")
         // console.log(index);
     }
-    function setsecdiv(data){
+    function setsecdiv(data) {
         setSecdiv(data);
+        check()
+
     }
 
     const [exp, setExp] = useState({
-        "title":"Experience",
-        "comp":[
+        "title": "Experience",
+        "comp": [
             {
-                "compname":"Microsoft",
-                "role":"SDE",
-                "ach":"i have won best employee award"
+                "compname": "Microsoft",
+                "role": "SDE",
+                "ach": "i have won best employee award"
             }
         ]
     })
-    function setexp(data){
-        setExp({...exp,comp:data});
+    function setexp(data) {
+        setExp({ ...exp, comp: data });
     }
-    function addexp(data){
-        setExp({...exp,"comp":[...exp.comp,data]});
+    function addexp(data) {
+        setExp({ ...exp, "comp": [...exp.comp, data] });
     }
     // console.log("index page role ",exp.comp[0].role)
 
     //fourth (eduction)
-    const [fourdiv,setFourdiv]=useState(false);
-    function setfourdiv(value){
+    const [fourdiv, setFourdiv] = useState(false);
+    function setfourdiv(value) {
         setFourdiv(value);
+        check()
+
     }
-    const [eduind,setEduind]=useState(-1);
-    function seteduind(i){
-         setEduind(i);
+    const [eduind, setEduind] = useState(-1);
+    function seteduind(i) {
+        setEduind(i);
     }
-    const [edu,setEdu]=useState({
-        "title":"Education",
-        "comp":[
+    const [edu, setEdu] = useState({
+        "title": "Education",
+        "comp": [
             {
-            "school":"mother pride school",
-            "subject":"primary education",
-            "ach":"i topped from bottom"
+                "school": "mother pride school",
+                "subject": "primary education",
+                "ach": "i topped from bottom"
             }
         ]
     })
-    function setedu(data){
-        setEdu({...edu,comp:data});
+    function setedu(data) {
+        setEdu({ ...edu, comp: data });
     }
-    function addedu(data){
-        setEdu({...edu,"comp":[...edu.comp,data]});
+    function addedu(data) {
+        setEdu({ ...edu, "comp": [...edu.comp, data] });
     }
 
     //Skills section
-    const [skill,setSkill]=useState(["full stack","devops"]);
-    function addskill(value){
-        setSkill([...skill,value]);
+    const [skill, setSkill] = useState(["full stack", "devops"]);
+    function addskill(value) {
+        setSkill([...skill, value]);
 
     }
-    function removeskill(ind){
-        console.log("here ",ind)
-           const updated=skill.filter((e,i)=>i !== ind);
-           setSkill(updated);
+    function removeskill(ind) {
+        console.log("here ", ind)
+        const updated = skill.filter((e, i) => i !== ind);
+        setSkill(updated);
     }
-    const[thirddiv,setThirddiv]=useState(false);
-    function setthirddiv(value){
-        console.log("i have been called ",value);
-           setThirddiv(value);
+    const [thirddiv, setThirddiv] = useState(false);
+    function setthirddiv(value) {
+        console.log("i have been called ", value);
+        setThirddiv(value);
+        check()
+
     }
 
     //fifth comp   languages
-    const [lang,setLang]=useState(["Java","Python","Javascript"]);
-    function addlang(value){
-        setLang([...lang,value]);
+    const [lang, setLang] = useState(["Java", "Python", "Javascript"]);
+    function addlang(value) {
+        setLang([...lang, value]);
     }
-    function removelang(ind){
-        const updated=lang.filter((e,i)=>i !== ind);
+    function removelang(ind) {
+        const updated = lang.filter((e, i) => i !== ind);
         setLang(updated);
     }
-    const[fifthdiv,setFifthdiv]=useState(false);
-    function setfifthdiv(value){
+    const [fifthdiv, setFifthdiv] = useState(false);
+    function setfifthdiv(value) {
         setFifthdiv(value);
+        check()
+
     }
     //sixth component awards
-    const [awards,setAwards]=useState(["nobel","punitzer"]);
-    function addawards(value){
-        setAwards([...awards,value]);
+    const [awards, setAwards] = useState(["nobel", "punitzer"]);
+    function addawards(value) {
+        setAwards([...awards, value]);
     }
-    function removeawards(ind){
-        const updated=awards.filter((e,i)=>i !== ind);
+    function removeawards(ind) {
+        const updated = awards.filter((e, i) => i !== ind);
         setAwards(updated);
     }
-    const [sixthdiv,setSixthdiv]=useState(false);
-    function setsixthdiv(value){
+    const [sixthdiv, setSixthdiv] = useState(false);
+    function setsixthdiv(value) {
         setSixthdiv(value);
+        check()
+
     }
 
     //seventh component awards
-    const [cert,setCert]=useState(["udemy certificate","courseera"])
-    function addcert(value){
-        setCert([...cert,value]);
+    const [cert, setCert] = useState(["udemy certificate", "courseera"])
+    function addcert(value) {
+        setCert([...cert, value]);
     }
-    function removecert(ind){
-        const updated=cert.filter((e,i)=>i !== ind);
+    function removecert(ind) {
+        const updated = cert.filter((e, i) => i !== ind);
         setCert(updated);
     }
-    const [sevendiv,setSevendiv]=useState(false);
-    function setsevendiv(value){
+    const [sevendiv, setSevendiv] = useState(false);
+    function setsevendiv(value) {
         setSevendiv(value);
+        check()
+
     }
 
-    
 
+    const resumeref=useRef();
+    const [rencon, setRencon] = useState(true);
+    function check() {
+        if (!firstdiv && !secdiv && !thirddiv && !fourdiv && !fifthdiv && !sixthdiv && !sevendiv) {
+            console.log("this is true ")
+            setRencon(false);
+        } else {
+            setRencon(true);
+            console.log("this is false ")
+        }
+    }
+    const handlePrint = useReactToPrint({
+        content: () => resumeref.current,
+        pageStyle: `
+          @page {
+            size:  220mm 300mm;
+            margin: 2mm 1mm 1mm 1mm;
+             /* Adjust the margin values as needed */
+          }
+          @media print {
+            body {
+              margin: 0;
+            }
+            
+          }
+        `,
+      });
 
 
 
     return (
         <div className={styles.main}>
-            {secdiv && <Formtwo exp={exp} setexp={setexp} secdiv={secdiv} setsecdiv={setsecdiv} ind={ind} addexp={addexp} />} 
-            {thirddiv && <Formthree skills={skill} setthirddiv={setthirddiv} addskill={addskill} removeskill={removeskill} />}
-            {fourdiv && <Formfour edu={edu} setedu={setedu} fourdiv={fourdiv} setfourdiv={setfourdiv} ind={eduind} addedu={addedu}/>}
-            {fifthdiv && <Formfive skills={lang} setfifthdiv={setfifthdiv} addskill={addlang} removeskill={removelang}/>}
-            {sixthdiv && <Formsix skills={awards} setfifthdiv={setsixthdiv} addskill={addawards} removeskill={removeawards}/>}
-            {sevendiv && <Formseven skills={cert} setfifthdiv={setsevendiv} addskill={addcert} removeskill={removecert}/>}
+            {secdiv ? (<Formtwo exp={exp} setexp={setexp} secdiv={secdiv} setsecdiv={setsecdiv} ind={ind} addexp={addexp} />) : (<span></span>)}
+            {thirddiv ? (<Formthree skills={skill} setthirddiv={setthirddiv} addskill={addskill} removeskill={removeskill} />) : (<span></span>)}
+            {fourdiv ? (<Formfour edu={edu} setedu={setedu} fourdiv={fourdiv} setfourdiv={setfourdiv} ind={eduind} addedu={addedu} />) : (<span></span>)}
+            {fifthdiv ? (<Formfive skills={lang} setfifthdiv={setfifthdiv} addskill={addlang} removeskill={removelang} />) : (<span></span>)}
+            {sixthdiv ? (<Formsix skills={awards} setfifthdiv={setsixthdiv} addskill={addawards} removeskill={removeawards} />) : (<span></span>)}
+            {sevendiv ? (<Formseven skills={cert} setfifthdiv={setsevendiv} addskill={addcert} removeskill={removecert} />) : (<span></span>)}
             {firstdiv &&
                 <div className={styles.divone}>
                     <div className={styles.divonewrapper}>
-                        
+
                         <form onSubmit={handleSubmitone}>
 
                             <div className={styles.inputcont}>
@@ -217,52 +258,57 @@ export default function Index() {
                                 />
                             </div>
                             <div className={styles.buttondiv}>
-                            <div className={styles.crossbutton} onClick={()=>{crossfunctionone()}}>
-                            &#10060;
+                                <div className={styles.crossbutton} onClick={() => { crossfunctionone() }}>
+                                    &#10060;
+                                </div>
+                                <button type="submit" value="Sign in" className={styles.savebutton}>Save</button>
+
+
                             </div>
-                            <button type="submit" value="Sign in" className={styles.savebutton}>Save</button>
 
-                    
-                        </div>
 
-                           
 
                         </form>
                     </div>
 
                 </div>}{
-                !firstdiv && !secdiv && !thirddiv && !fourdiv && !fifthdiv && !sixthdiv && !sevendiv &&
-                <div className={styles.mainclass}>
-                    <div className={styles.heading}>
-                        Resume
-                    </div>
-                    <div className={styles.wrapper}>
-                        <div className={styles.template}>
-                            <div className={styles.firstmain} onClick={() => { funone() }} >
-                                <div className={styles.first}>
-                                    {names.username}
+                rencon ? (
+                    <div className={styles.mainclass}>
+                        <div className={styles.heading}>
+                            Resume
+                        </div>
+                        <div className={styles.wrapper}>
+                            <div className={styles.template} ref={resumeref}>
+                                <div className={styles.firstmain} onClick={() => { funone() }} >
+                                    <div className={styles.first}>
+                                        {names.username}
+                                    </div>
+                                    <div className={styles.second}>
+                                        {names.gmail} | {names.city},{names.state}
+                                    </div>
                                 </div>
-                                <div className={styles.second}>
-                                    {names.gmail} | {names.city},{names.state}
-                                </div>
+                                <Onetwo data={exp} secdiv={secdiv} setsecdiv={setsecdiv} setind={setind} />
+                                <Onethree skill={skill} addskill={addskill} removeskill={removeskill} setthirddiv={setthirddiv} />
+                                <Onefour data={edu} fourdiv={fourdiv} setfourdiv={setfourdiv} setind={seteduind} />
+                                <Onefive skill={lang} addskill={addlang} removeskill={removelang} setfifthdiv={setfifthdiv} />
+                                <Onesix skill={awards} addskill={addawards} removeskill={removeawards} setsixthdiv={setsixthdiv} />
+                                <Oneseven skill={cert} addskill={addcert} removeskill={removecert} setsevendiv={setsevendiv} />
+
+
+
+
                             </div>
-                             <Onetwo data={exp} secdiv={secdiv} setsecdiv={setsecdiv} setind={setind} />
-                             <Onethree skill={skill} addskill={addskill} removeskill={removeskill} setthirddiv={setthirddiv}/>
-                             <Onefour data={edu} fourdiv={fourdiv} setfourdiv={setfourdiv} setind={seteduind}/>
-                             <Onefive skill={lang} addskill={addlang} removeskill={removelang} setfifthdiv={setfifthdiv}/>
-                             <Onesix skill={awards} addskill={addawards} removeskill={removeawards} setsixthdiv={setsixthdiv}/>
-                             <Oneseven skill={cert} addskill={addcert} removeskill={removecert} setsevendiv={setsevendiv}/>
-                             
-                            
-                            
+                            <div className={styles.operation}>
+                                {/* <ReactToPrint
+                                    trigger={() => <button>Download</button>}
+                                    content={() => resumeref.current}
+                                /> */}
+                                <button onClick={handlePrint}>Download</button>
 
+                            </div>
                         </div>
-                        <div className={styles.operation}>
 
-                        </div>
-                    </div>
-
-                </div>
+                    </div>) : (<span></span>)
             }
 
 

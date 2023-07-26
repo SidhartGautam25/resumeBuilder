7c12e8490584c01b2effe90742dcab633f03370a
isham-26 <ishamgupta711@gmail.com>
2023-07-27 03:29:25 +0530

    materialicon remove

--- app/components/tempthird/compfour/Compfour.jsx
+++ app/components/tempthird/compfour/Compfour.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import styles from "./styles.module.css";
-import FilterNoneIcon from '@material-ui/icons/FilterNone';
 const Compfour = () => {
   return (
     <>
     <div className={styles.container}>
          <h3 style={{color:"rgb(0, 87, 187)"}}>WORK EXPERIANCE</h3>
          <span className={styles.work}>Fullstack devleper</span>
-         <div className={styles.icon}><span>AirState Solution</span><FilterNoneIcon/></div>
+         <div className={styles.icon}><span>AirState Solution</span></div>
          <div className={styles.stay}><span>09/2015-10/2020</span><span>Adress Lucknow U.P</span></div>
          <div className={styles.list}>
             {/* <ul> */}
@@ -21,7 +20,7 @@ const Compfour = () => {
     </div>
     <div className={styles.container2}>
          <span className={styles.work2}>Development Assistant</span>
-         <div className={styles.icon}><span>AirState Solution</span><FilterNoneIcon/></div>
+         <div className={styles.icon}><span>AirState Solution</span></div>
          <div className={styles.stay}><span>09/2015-10/2020</span><span>Adress Lucknow U.P</span></div>
          <div className={styles.list}>
             {/* <ul> */}

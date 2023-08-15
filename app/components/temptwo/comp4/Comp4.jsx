750b5a3b5d07a756523e4cf14f92d1a2be0f23d8
Ankit <ankitdj13112002@gmail.com>
2023-08-15 15:58:05 +0530

    few changes

--- app/components/temptwo/comp4/Comp4.jsx
+++ app/components/temptwo/comp4/Comp4.jsx
@@ -41,64 +41,443 @@ export default function Comp4() {
         </div>
         <div className={styles.info}>
           <div className={styles.position}>
-            project managment certificate from wipro project managment project
-            managment certificate from wipro project managment certificate from
-            wipro project managment certificate from wipro certificate from
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro project managment cert
-            ificate from wipro project managment certificate from wipro wipro
-            project managment certificate from wipro certificate from wipro
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro project managment
-            certificate from wipro project managment certificate from wipro
-            project managment certificate from wipro project managment
-            certificate from wipro project managment certificate from wipro
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro project managment cert
-            ificate from wipro project managment certificate from wipro wipro
-            project managment certificate from wipro certificate from wipro
-            project managment certificate from wipro project managment
-            certificate from wipro project managment certificate from wipro
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            project managment certificate from wipro project managment project
-            managment certificate from wipro certificate from wipro wipro wipro
-            project managment certificate from wipro project managment
-            certificate from wipro project managment certificate from wipro
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            project managment certificate from wipro project managment project
-            managment certificate from wipro certificate from wipro wipro wipro
-            project managment certificate from wipro project managment
-            certificate from wipro project managment certificate from wipro
-            project managment certificate from wipro project managment
-            certificate from wipro project managment certificate from wipro
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            project managment certificate from wipro project managment
-            certificate from wipro project managment certificate from wipro
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            project managment certificate from wipro project managment project
-            managment certificate from wipro certificate from wipro wipro wipro
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            project managment certificate from wipro project managment project
-            project managment certificate from wipro managment certificate from
-            project managment certificate from wipro wipro certificate from
-            wipro wipro wipro
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
+          </div>
+        </div>
+      </div>
+      <div className={styles.expe}>
+        <div className={styles.date}>
+          <span>2001-05</span>
+        </div>
+        <div className={styles.info}>
+          <div className={styles.position}>
+            project managment certificate from wipro
           </div>
         </div>
       </div>
